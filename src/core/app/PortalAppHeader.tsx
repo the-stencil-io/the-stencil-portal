e6f59904db59871769118ef1889cd0a11c5318de
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2021-12-21 15:41:22 +0200

    PortalAppHeader

--- src/core/app/PortalAppHeader.tsx
+++ src/core/app/PortalAppHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Button } from '@mui/material';
 
 import MenuIcon from '@mui/icons-material/Menu';
 import PersonIcon from '@mui/icons-material/Person';
@@ -7,40 +7,54 @@ import Portal from '../'
 import logo from './logo/burger.png';
 
 
-const PortalAppToolbar: React.FC<{}> = () => {
+// username and language select
+const UserOptionsBar: React.FC<{}> = () => {
+  return (
+    <Box display="flex" sx={{ backgroundColor: 'primary.main' }}>
+      <Box sx={{ ml: 1 }}> John Smith </Box>
+      <Box flexGrow={1} />
+      <Box><Button sx={{ color: 'primary.contrastText', p: 0 }}>EN</Button></Box>
+    </Box>
+  )
+}
+
+// logo, user login, menu toggle
+const LogoBar: React.FC<{}> = () => {
   const { actions, session } = Portal.useDrawer();
 
   return (
+    <Box display="flex" color="black" >
+      <Box alignSelf="center"
+        sx={{ color: 'primary.main', ml: 1 }}
+        flexGrow={1}>
+        <img src={logo} height="70px" width="70px" />
+      </Box>
+
+      <Box flexGrow={1} />
+
+      <Box display="flex" alignSelf="center" >
+        <Box>
+          <IconButton sx={{ color: "primary.main" }}>
+            <PersonIcon />
+          </IconButton>
+        </Box>
+        <Box>
+          <IconButton sx={{ color: "primary.main" }} onClick={() => actions.handleDrawerOpen(!session.drawer)}>
+            <MenuIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+
+const PortalAppToolbar: React.FC<{}> = () => {
+
+  return (
     <Box width="100%" height="100%" display="flex" flexDirection="column">
-
-      <Box display="flex" sx={{ backgroundColor: 'primary.main'}}>
-        <Box sx={{ml:1}}> John Smith </Box>
-        <Box flexGrow={1} />
-        <Box sx={{mr:1}}>EN</Box>
-      </Box>
-
-      <Box display="flex" color="black" >
-        <Box alignSelf="center"
-          sx={{ color: 'primary.main', ml: 1 }}
-          flexGrow={1}>
-          <img src={logo} height="70px" width="70px" />
-        </Box>
-        
-        <Box flexGrow={1} />
-
-        <Box display="flex" alignSelf="center" sx={{mr: 1}} >
-          <Box>
-            <IconButton  sx={{color:"primary.main"}}>
-              <PersonIcon />
-            </IconButton>
-          </Box>
-          <Box>
-            <IconButton  sx={{color:"primary.main"}} onClick={() => actions.handleDrawerOpen(!session.drawer)}>
-              <MenuIcon />
-            </IconButton>
-          </Box>
-        </Box>
-      </Box>
+      <UserOptionsBar />
+      <LogoBar />
     </Box>
   )
 }
