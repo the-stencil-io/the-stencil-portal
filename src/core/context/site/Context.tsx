8fde97064ff34487e724c897f4aa0b73ccfc9bdc
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2022-01-21 12:01:15 +0200

    overrides

--- src/core/context/site/Context.tsx
+++ src/core/context/site/Context.tsx
@@ -28,12 +28,6 @@ const SiteProvider: React.FC<SiteProviderProps> = (props) => {
   const actions = React.useMemo(() => new SiteReducerDispatch(dispatch), [dispatch]);
   const contextValue = React.useMemo(() => initContext(state, service, actions), [state, service, actions]);
 
-  React.useEffect(() => {
-    console.log("portal: reinit overriddes");
-    dispatch({ type: "setOverrides",  overrides });
-  }, [overrides]);
-
-
   // load site
   React.useEffect(() => {
     if (!state.loaded) {
